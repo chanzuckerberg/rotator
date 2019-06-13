0900c4a2f957f0fe78a447233d95ac75ada43ba6
redhoteggplant <nadia.irwanto@yale.edu>
2019-06-13 13:44:02 -0700

    config basic format

---
+++ pkg/config/config.go
@@ -0,0 +1,39 @@
+package config
+
+import (
+	"io/ioutil"
+
+	"github.com/pkg/errors"
+	"gopkg.in/yaml.v2"
+)
+
+type Config struct {
+	Version int      `yaml:"version"`
+	Secrets []Secret `yaml:"secrets"`
+}
+
+type Secret struct {
+	Name         string        `yaml:"name"`
+	Source       Source        `yaml:"source"`
+	Destinations []Destination `yaml:"destinations"`
+}
+
+type Source struct {
+	Kind string `yaml:"kind"`
+}
+
+type Destination struct {
+	Kind string `yaml:"kind"`
+}
+
+func FromFile(file string) (*Config, error) {
+	b, err := ioutil.ReadFile(file)
+	if err != nil {
+		return nil, err
+	}
+
+	conf := &Config{}
+	err = yaml.Unmarshal(b, conf)
+
+	return conf, errors.Wrap(err, "Could not unmarshal config")
+}
