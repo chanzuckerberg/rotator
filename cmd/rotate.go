1c5266e42e267594e9f4afc34c076b68a23f3df6
Nadia <nadia.irwanto@yale.edu>
2019-07-23 15:18:42 -0700

    run plan and prompt user before rotating secrets (#13)
    run plan and prompt user before rotating secrets* `rotate` cmd prints a plan, and prompts the user before continuing with the rotation
    * If the `-y` flag is set, `rotate` assumes yes as an answer to all prompts and runs non-interactively, skipping all prompts
    
    ### References
    * Prompt messages are adapted from the `terraform apply` command
    * `-y` flag is inspired by `apt-get` https://askubuntu.com/questions/805067/is-there-a-way-to-force-yes-to-any-prompts-when-installing-from-apt-get-from
    
    ### Screenshot of terminal
    ![image](https://user-images.githubusercontent.com/21990896/61751123-1c01fe80-ad5c-11e9-9919-cd4b6d671380.png)

--- cmd/rotate.go
+++ cmd/rotate.go
@@ -5,13 +5,17 @@ import (
 	"fmt"
 
 	"github.com/chanzuckerberg/rotator/pkg/config"
+	"github.com/fatih/color"
 	"github.com/hashicorp/go-multierror"
 	"github.com/pkg/errors"
+	"github.com/segmentio/go-prompt"
+	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 )
 
 func init() {
 	rotateCmd.Flags().StringP("file", "f", "", "Config file to read from")
+	rotateCmd.Flags().BoolP("yes", "y", false, "Assume \"yes\" to all prompts and run non-interactively.")
 	rootCmd.AddCommand(rotateCmd)
 }
 
@@ -20,36 +24,84 @@ var rotateCmd = &cobra.Command{
 	Short: "Rotate secrets",
 	Long: `rotate parses a config file, rotates the secret at 
 			the source, and writes the new secret to each sink`,
+	SilenceErrors: true, // If we don't silence here, cobra will print them. But we want to do that in cmd/root.go
 	RunE: func(cmd *cobra.Command, args []string) error {
-		// handle flags
+		// parse config and print plan
 		file, err := cmd.Flags().GetString("file")
 		if err != nil {
 			return errors.Wrap(err, "unable to parse config flag")
 		}
-
 		config, err := config.FromFile(file)
 		if err != nil {
 			return errors.Wrap(err, "unable to read config from file")
 		}
+		printPlan(config)
+
+		// prompt user to continue if necessary
+		skipPrompt, err := cmd.Flags().GetBool("yes")
+		if err != nil {
+			return errors.Wrap(err, "unable to parse yes flag")
+		}
+		if !(skipPrompt || getPrompt()) {
+			return nil
+		}
+
+		// rotate secrets
+		logrus.Println("Performing the actions described above.")
 		return RotateSecrets(config)
 	},
+}
+
+func printPlan(config *config.Config) {
+	for _, secret := range config.Secrets {
+		logrus.Println("Rotating secret", secret.Name)
+
+		src := secret.Source
+		logrus.Println("*", "reading new credentials from", src.Kind(), "source")
+
+		for _, sink := range secret.Sinks {
+			logrus.Println("*", "writing new credentials to", sink.Kind(), "sink")
+		}
+		logrus.Println()
+	}
+}
+
+func getPrompt() bool {
+	// print prompt
+	b := color.New(color.Bold).SprintFunc()
+	logrus.Println(b("Do you want to perform these actions?"))
+	logrus.Println("  rotator will perform the actions described above.")
+	logrus.Println("  Yes', 'yes', 'y', 'Y' will be accepted to approve.")
+	logrus.Println("  No', 'no', 'n', 'N' will cancel the rotation.")
+	logrus.Println()
+
+	// get user input
+	yes := prompt.Confirm(b("  Enter a value"))
+	logrus.Println()
+	if !yes {
+		redB := color.New(color.FgRed, color.Bold).SprintFunc()
+		logrus.Errorln()
+		logrus.Errorln(redB("Error: "), "Rotation cancelled.")
+		logrus.Errorln()
+	}
+	return yes
 }
 
 // RotateSecrets takes a config, reads the secret from the source,
 // and writes it to each sink.
 func RotateSecrets(config *config.Config) error {
+	var errs *multierror.Error
 	ctx := context.Background()
 	for _, secret := range config.Secrets {
-		var errs *multierror.Error
-
 		// Rotate credential at source
 		src := secret.Source
 		newCreds, err := src.Read()
 		if err != nil {
-			return errors.Wrapf(err, "%s: unable to rotate secret at %s", secret.Name, src.Kind())
+			errs = multierror.Append(errs, errors.Wrapf(err, "%s: unable to rotate secret at %s", secret.Name, src.Kind()))
+			continue
 		}
-		if newCreds == nil {
-			return nil
+		if newCreds == nil { // not time to rotate yet
+			continue
 		}
 
 		// Write new credentials to each sink
@@ -73,5 +125,5 @@ func RotateSecrets(config *config.Config) error {
 			}
 		}
 	}
-	return nil
+	return errs.ErrorOrNil()
 }
--- go.mod
+++ go.mod
@@ -3,18 +3,27 @@ module github.com/chanzuckerberg/rotator
 go 1.12
 
 require (
+	github.com/airbrake/gobrake v3.7.4+incompatible
 	github.com/aws/aws-sdk-go v1.20.6
+	github.com/caio/go-tdigest v2.3.0+incompatible // indirect
 	github.com/chanzuckerberg/go-misc v0.0.0-20190716060611-91021143eac9
-	github.com/davecgh/go-spew v1.1.1
+	github.com/fatih/color v1.7.0
 	github.com/google/go-querystring v1.0.0 // indirect
 	github.com/hashicorp/go-multierror v1.0.0
+	github.com/howeyc/gopass v0.0.0-20170109162249-bf9dde6d0d2c // indirect
 	github.com/kr/pretty v0.1.0 // indirect
+	github.com/leesper/go_rng v0.0.0-20190531154944-a612b043e353 // indirect
+	github.com/mattn/go-colorable v0.1.2 // indirect
+	github.com/onsi/ginkgo v1.8.0 // indirect
+	github.com/onsi/gomega v1.5.0 // indirect
 	github.com/pkg/errors v0.8.0
+	github.com/segmentio/go-prompt v1.2.1-0.20161017233205-f0d19b6901ad // build fails otherwise
 	github.com/shuheiktgw/go-travis v0.2.2
 	github.com/sirupsen/logrus v1.4.2
 	github.com/spf13/cobra v0.0.5
 	github.com/stretchr/testify v1.2.2
 	golang.org/x/net v0.0.0-20190620200207-3b0461eec859 // indirect
+	gonum.org/v1/gonum v0.0.0-20190723192742-2122b538b6f0 // indirect
 	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
 	gopkg.in/yaml.v2 v2.2.2
 )
