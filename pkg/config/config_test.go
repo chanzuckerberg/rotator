6ec536081cbdf709606e7f340e59e5fde001979a
Annie <anne.ku@chanzuckerberg.com>
2021-01-27 11:36:45 -0800

    save progress on testing listsource

--- pkg/config/config_test.go
+++ pkg/config/config_test.go
@@ -112,24 +112,24 @@ func TestConfigWithLists(t *testing.T) {
 	r.Equal(c1, c2)
 }
 
-func TestConfigWithCustomStructs(t *testing.T) {
-	r := require.New(t)
-	tmpFile, err := ioutil.TempFile("", "tmpConfig")
-	r.Nil(err)
-	defer tmpFile.Close()
-	defer os.Remove(tmpFile.Name())
+// func TestConfigWithCustomStructs(t *testing.T) {
+// 	r := require.New(t)
+// 	tmpFile, err := ioutil.TempFile("", "tmpConfig")
+// 	r.Nil(err)
+// 	defer tmpFile.Close()
+// 	defer os.Remove(tmpFile.Name())
 
-	c1 := &config.Config{Secrets: []config.Secret{
-		// Add content here!!!
-	}}
-	// Marshal (just single key-pair values)
-	bytes, err := yaml.Marshal(c1)
-	r.Nil(err)
-	_, err = tmpFile.Write(bytes)
-	r.Nil(err)
-	// read file
-	c2, err := config.FromFile(tmpFile.Name())
-	r.Nil(err)
+// 	c1 := &config.Config{Secrets: []config.Secret{
+// 		// Add content here!!!
+// 	}}
+// 	// Marshal (just single key-pair values)
+// 	bytes, err := yaml.Marshal(c1)
+// 	r.Nil(err)
+// 	_, err = tmpFile.Write(bytes)
+// 	r.Nil(err)
+// 	// read file
+// 	c2, err := config.FromFile(tmpFile.Name())
+// 	r.Nil(err)
 
-	r.Equal(c1, c2)
-}
+// 	r.Equal(c1, c2)
+// }
